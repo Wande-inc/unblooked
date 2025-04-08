b4c23774296d6a97b688014565581231f613bb2a
W&E inc. <wande.inc@gmail.com>
2025-04-08 08:39:22 -0500

    Create index.js

---
+++ client/basketball-stars/index.js
@@ -0,0 +1,69 @@
+document.getElementById('iframe').srcdoc = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta
+      name="viewport"
+      content="initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/gh/Wande-inc/unblooked/assets/games/basketball-stars/assets/css/app.css"
+      type="text/css"
+    />
+    <title>...</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  </head>
+
+  <body>
+    <div id="content"></div>
+    <div id="orientation"></div>
+    <div id="loader">Loading ...</div>
+
+    <script
+      type="text/javascript"
+      src="https://cdn.jsdelivr.net/gh/Wande-inc/unblooked/assets/games/basketball-stars/assets/lib/nape.min.js"
+    >
+      var nape = "nape.min.js";
+    </script>
+
+    <script
+      type="text/javascript"
+      src="https://cdn.jsdelivr.net/gh/Wande-inc/unblooked/assets/games/basketball-stars/assets/lib/jquery-3.1.1.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="https://cdn.jsdelivr.net/gh/Wande-inc/unblooked/assets/games/basketball-stars/assets/lib/easeljs-0.8.2.combined.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="https://cdn.jsdelivr.net/gh/Wande-inc/unblooked/assets/games/basketball-stars/assets/lib/bluebird.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="https://cdn.jsdelivr.net/gh/Wande-inc/unblooked/assets/games/basketball-stars/assets/lib/phaser.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="https://cdn.jsdelivr.net/gh/Wande-inc/unblooked/assets/games/basketball-stars/assets/lib/phaser-cachebuster.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="https://cdn.jsdelivr.net/gh/Wande-inc/unblooked/assets/games/basketball-stars/assets/lib/phaser-super-storage.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="https://cdn.jsdelivr.net/gh/Wande-inc/unblooked/assets/games/basketball-stars/assets/lib/dragonBones.min.js"
+    ></script>
+
+    <script type="text/javascript">
+      var notIE11 = typeof Event === "function";
+    </script>
+    <script
+      type="text/javascript"
+      src="https://cdn.jsdelivr.net/gh/Wande-inc/unblooked/assets/games/basketball-stars/1.min.js"
+    ></script>
+  </body>
+</html>`
